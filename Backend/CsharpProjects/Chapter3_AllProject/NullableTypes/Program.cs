1d25a3aa7659b01b7e1e968958826b7f9f45388e
Charlton Adams <charltonadams38@gmail.com>
2025-01-17 13:33:38 +0200

    MAUI Getting started

---
+++ Backend/CsharpProjects/Chapter3_AllProject/NullableTypes/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+static void LocalNullableTypes()
+{
+    int? nullableint = 10;
+    double? nullableDouble = 3.14;
+    bool? nullableBool = null;
+    int?[] arrayOfNullableInts = new int?[10];
+    Console.WriteLine("Nullable Types:");
+    Console.WriteLine("Value of nullableint: {0}", nullableint);
+}
+
+static void LocalNullableTypesUsingNullable()
+{
+    // Define some local nullable types using Nullable<T>.
+    Nullable<int> nullableint = 10;
+    Nullable<double> nullableDouble = 3.14;
+    Nullable<bool> nullableBool = null;
+    Console.WriteLine("Using Nullable<T>:");
+    Console.WriteLine("Value of nullableint: {0}", nullableint);
+}
+
