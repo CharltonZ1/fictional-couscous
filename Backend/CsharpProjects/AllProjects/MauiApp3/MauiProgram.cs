e7948ec6599a05332b1e41fae0228ce6b33ac4b5
Charlton Adams <charltonadams38@gmail.com>
2025-02-03 17:53:13 +0200

    Add some base styling and fonts for .NET MAUI app

--- Backend/CsharpProjects/AllProjects/MauiApp3/MauiProgram.cs
+++ Backend/CsharpProjects/AllProjects/MauiApp3/MauiProgram.cs
@@ -11,8 +11,10 @@ namespace MauiApp3
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    //fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    //fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("PFBeauSansPro-Regular.ttf", "PFBeauSansProRegular");
+                    fonts.AddFont("PFBeauSansPro-SemiBold.ttf", "PFBeauSansProSemiBold");
                 });
 
 #if DEBUG
